16326c7110330e16efd5858ee659b8f3d237b5bf
SSDisplay2017 <chareau@chareaus-Mini.local.tld>
2017-04-12 14:27:36 -0400

    Merge branch 'master' into AlexIntegration
    Should be fine now

--- sphere/sign-apk.sh
+++ sphere/sign-apk.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #sign jar file
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
-      release-unsigned.apk myapp \
+      platforms/android/build/output/apk/android-release-unsigned.apk myapp \
       -keystore ../../bin/keystore \
       -storepass 123456
 #remove old signed version
-rm -rfv myapp.apk
+# rm -rfv myapp.apk
 #pack new apk
-$ANDROID_HOME/build-tools/25.0.2/zipalign 4 release-unsigned.apk myapp.apk
+$ANDROID_HOME/build-tools/25.0.2/zipalign 4 platforms/android/build/output/apk/android-release-unsigned.apk myapp \
+       platforms/android/build/output/apk/myapp.apk 
 
 # zipalign 4 release-unsigned.apk myapp.apk
 #remove old unsigned version

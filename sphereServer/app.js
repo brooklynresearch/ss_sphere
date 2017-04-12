eb6f305b20dc174f78862945902089343c6f9c3d
Jonothy <johnny@brooklynresearch.org>
2017-04-12 13:51:12 -0400

    controller app
    This is a basic integration of the controller app

--- sphereServer/app.js
+++ sphereServer/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var routeController = require('./routes/controller');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/controller', routeController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

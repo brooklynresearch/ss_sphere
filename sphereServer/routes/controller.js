e35f6f35bd9826d321d109a5341e91fb2433738f
pbscrimmage <patrick@brooklynresearch.org>
2017-05-04 18:05:05 -0400

    no more websockets for position updates, controller route gets env vars

--- sphereServer/routes/controller.js
+++ sphereServer/routes/controller.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 // liberty, times square, blossoms
-var timeFrames = [2, 8, 32, 0];
-var names = ['Liberty', 'Times Square', 'Central Park', 'Logo'];
+//var timeFrames = [ 7, 7, 7, 7 ];
+//var names = ['Liberty', 'Times Square', 'Central Park', 'Logo'];
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var names = process.env.POSITION_NAMES.split()
+  var posStrings = process.env.VIDEO_POSITIONS.split()
   res.render('controller', { title: 'Express', 
-  		vid1: timeFrames[0], 
-  		vid2: timeFrames[1], 
-  		vid3: timeFrames[2],
-  		vid4: timeFrames[3],
+  		vid1: posStrings[0], 
+  		vid2: posStrings[1], 
+  		vid3: posStrings[2],
+  		vid4: posStrings[3],
   		name1: names[0],
   		name2: names[1],
   		name3: names[2],

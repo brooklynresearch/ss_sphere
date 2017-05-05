27d93c6052078fbcd3fa102e76b38e796ac4e186
pbscrimmage <patrick@brooklynresearch.org>
2017-05-05 16:50:07 -0400

    controller route reads config file on every /controller request

--- sphereServer/routes/controller.js
+++ sphereServer/routes/controller.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var db = require('../db');
-// liberty, times square, blossoms
-var timeFrames = [ 7, 7, 7, 7 ];
-var names = ['Liberty', 'Times Square', 'Central Park', 'Logo'];
+const exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //var names = process.env.POSITION_NAMES.split()
-  //var posStrings = process.env.VIDEO_POSITIONS.split()
-  res.render('controller', { title: 'Express', 
-  		vid1: posStrings[0], 
-  		vid2: posStrings[1], 
-  		vid3: posStrings[2],
-  		vid4: posStrings[3],
-  		name1: names[0],
-  		name2: names[1],
-  		name3: names[2],
-  		name4: names[3]
-  	});
+
+    exec("cat ./videofile.config | grep -e NAMES -e POSITIONS | sed -e 's/[=\"]/ /g' | awk '{print $2}'",
+            (err, stdout, stderr) => {
+
+            if (err) {
+                console.log("ERROR: ", err);
+            }
+            var lines = stdout.split('\n');
+            var posStrings = lines[0].split(',');
+            var names = lines[1].split(',');
+
+            res.render('controller', { title: 'Controller',
+                vid1: posStrings[0],
+                vid2: posStrings[1],
+                vid3: posStrings[2],
+                vid4: posStrings[3],
+                name1: names[0],
+                name2: names[1],
+                name3: names[2],
+                name4: names[3]
+            });
+    });
 });
 
 module.exports = router;

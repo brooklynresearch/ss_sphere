7ec6d2226ad03910888462f362910335aa6e3003
pbscrimmage <patrick@brooklynresearch.org>
2018-09-27 22:03:45 -0400

    mapping full image texture

--- sphereNative/app/src/main/java/com/bkr/spherenative/GLCanvas.java
+++ sphereNative/app/src/main/java/com/bkr/spherenative/GLCanvas.java
@@ -1,7 +1,13 @@
 package com.bkr.spherenative;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
+import android.os.Environment;
 
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -10,21 +16,34 @@ import java.nio.ShortBuffer;
 import static android.opengl.GLES20.glCreateProgram;
 
 public class GLCanvas {
+    private static File DOWNLOAD_DIR =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
     private FloatBuffer vertexBuffer;
+    private FloatBuffer texCoordBuffer;
     private ShortBuffer drawListBuffer;
 
     // number of coordinates per vertex in this array
-    static final int COORDS_PER_VERTEX = 3;
-    static float squareCoords[] = {
+    private static final int COORDS_PER_VERTEX = 3;
+    private static float squareCoords[] = {
             -1.0f,  1.0f, 0.0f,   // top left
             -1.0f, -1.0f, 0.0f,   // bottom left
             1.0f, -1.0f, 0.0f,   // bottom right
             1.0f,  1.0f, 0.0f }; // top right
 
+    private static float texCoords[] = {  //have to flip Y axis because bitmap coords go top -> bottom
+        0.0f, 0.0f, // top left
+        0.0f, 1.0f, // bottom left
+        1.0f, 1.0f, // bottom right
+        1.0f, 0.0f, // top right
+    };
+
     private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
 
     private int positionHandle;
-    private int colorHandle;
+    //private int colorHandle;
+    private int texUniformHandle;
+    private int texCoordHandle;
 
     float color[] = { 0.63671875f, 0.22265625f, 0.76953125f, 1.0f };
 
@@ -34,18 +53,24 @@ public class GLCanvas {
 
     private final String vertexShaderCode =
             "attribute vec4 vPosition;" +
+            "attribute vec2 aTexCoordinate;" +
+            "varying vec2 vTexCoordinate;" +
                     "void main() {" +
+                    "  vTexCoordinate = aTexCoordinate;" +
                     "  gl_Position = vPosition;" +
                     "}";
 
     private final String fragmentShaderCode =
             "precision mediump float;" +
-                    "uniform vec4 vColor;" +
+            "uniform sampler2D uTexture;" +
+            "varying vec2 vTexCoordinate;" +
+            //"uniform vec4 vColor;" +
                     "void main() {" +
-                    "  gl_FragColor = vColor;" +
+                    "  gl_FragColor = texture2D(uTexture, vTexCoordinate);" +
                     "}";
 
     private final int program;
+    private final int texture;
 
     public GLCanvas() {
         // initialize vertex byte buffer for shape coordinates
@@ -68,6 +93,12 @@ public class GLCanvas {
         // set the buffer to read the first coordinate
         drawListBuffer.position(0);
 
+        ByteBuffer tbb = ByteBuffer.allocateDirect(texCoords.length * 4);
+        tbb.order(ByteOrder.nativeOrder());
+        texCoordBuffer = tbb.asFloatBuffer();
+        texCoordBuffer.put(texCoords);
+        texCoordBuffer.position(0);
+
         int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
 
         int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
@@ -83,11 +114,24 @@ public class GLCanvas {
 
         // creates OpenGL ES program executables
         GLES20.glLinkProgram(program);
+
+        Uri path = Uri.fromFile(new File(DOWNLOAD_DIR + "/" + "wash-sq.jpeg"));
+        texture = loadTexture(path);
     }
 
     public void draw() {
         // Add program to OpenGL ES environment
         GLES20.glUseProgram(program);
+
+        texUniformHandle = GLES20.glGetUniformLocation(program, "uTexture");
+        // Set the active texture unit to texture unit 0.
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+
+        // Bind the texture to this unit.
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture);
+
+        // Tell the texture uniform sampler to use this texture in the shader by binding to texture unit 0.
+        GLES20.glUniform1i(texUniformHandle, 0);
 
         // get handle to vertex shader's vPosition member
         positionHandle = GLES20.glGetAttribLocation(program, "vPosition");
@@ -100,11 +144,17 @@ public class GLCanvas {
                 GLES20.GL_FLOAT, false,
                 vertexStride, vertexBuffer);
 
+        texCoordHandle = GLES20.glGetAttribLocation(program, "aTexCoordinate");
+        GLES20.glEnableVertexAttribArray(texCoordHandle);
+
+        GLES20.glVertexAttribPointer(texCoordHandle, 2, GLES20.GL_FLOAT, false,
+                2*4, texCoordBuffer);
+
         // get handle to fragment shader's vColor member
-        colorHandle = GLES20.glGetUniformLocation(program, "vColor");
+        //colorHandle = GLES20.glGetUniformLocation(program, "vColor");
 
         // Set color for drawing the sqaure
-        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+        //GLES20.glUniform4fv(colorHandle, 1, color, 0);
 
         // Draw the triangle
         //GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
@@ -127,6 +177,34 @@ public class GLCanvas {
         GLES20.glCompileShader(shader);
 
         return shader;
+    }
+
+    private static int loadTexture(Uri fileUri) {
+        final int[] textureHandle = new int[1];
+        GLES20.glGenTextures(1, textureHandle, 0);
+
+        if (textureHandle[0] != 0)
+        {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inScaled = false;   // No pre-scaling
+
+            // Read in the resource
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(), options);
+
+            // Bind to the texture in OpenGL
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
+
+            // Set filtering
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+            // Load the bitmap into the bound texture.
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+            // Recycle the bitmap, since its data has been loaded into OpenGL.
+            bitmap.recycle();
+        }
+        return textureHandle[0];
     }
 
 }
